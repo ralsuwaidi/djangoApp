7325284f497da7172bb89e16a482d61264307120
Ras <x.suwaidi@gmail.com>
2018-04-14 21:39:14 +0400

    cleaned up text

--- vivian/functions.py
+++ vivian/functions.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from .models import Writingprompt
 
 def reddit():
-    
+
     reddit = praw.Reddit(client_id='T1mKRsAWXfGfzA',
                          client_secret='9I0sxWA2kmIDi4TIaP7LLvON0zA',
                          user_agent='my user agent')
@@ -12,9 +12,10 @@ def reddit():
 
     stickies = [reddit.subreddit("writingprompts").sticky(i).id for i in range(1,2)]
 
-    for submission in reddit.subreddit('writingprompts').hot(limit=5):
+    for submission in reddit.subreddit('writingprompts').hot(limit=3):
         if submission.id not in stickies:
             date = datetime.utcfromtimestamp(submission.created_utc)
             top_comment = [comment.body for comment in submission.comments if (hasattr(comment, "body") and comment.distinguished==None)][0]
             Writingprompt.objects.update_or_create(title=submission.title, comment=top_comment,
             defaults={'score':submission.score, 'pub_date':date},)
+            return top_comment
--- vivian/views.py
+++ vivian/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Writingprompt
 from .functions import reddit
+import markdown2
 
 
 def index(request):
-    v=reddit()
+    v=markdown2.markdown(reddit())
     latest_question_list = Writingprompt.objects.all()
-    return render(request, 'vivian/index.html', {'question': latest_question_list})
+    return render(request, 'vivian/index.html', {'question': latest_question_list, 'raw_comment':v})
