1398b0a59e0edcf77c5d3259181be0033e88e84c
Ras <x.suwaidi@gmail.com>
2018-04-14 12:28:59 +0400

    hi

--- vivian/views.py
+++ vivian/views.py
@@ -3,12 +3,8 @@ from django.http import HttpResponse
 from .models import Writingprompt
 from .functions import reddit
 
-<<<<<<< HEAD
 
 def index(request):
     v=reddit()
-=======
-def index(request):
->>>>>>> b6a1f70d0fb60e7eeee3ed739e3ac89a12228be2
     latest_question_list = Writingprompt.objects.all()
     return render(request, 'vivian/index.html', {'question': latest_question_list})
