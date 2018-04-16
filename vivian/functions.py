0fbb734f3fa09d6be402907cd800d369c5132f4d
real4sight <x_suwaidi@hotmail.com>
2018-04-16 10:06:20 +0400

    beutify

--- vivian/functions.py
+++ vivian/functions.py
@@ -2,21 +2,24 @@ import praw
 from datetime import datetime
 from .models import Writingprompt
 
+
 def reddit():
 
     reddit = praw.Reddit(client_id='T1mKRsAWXfGfzA',
                          client_secret='9I0sxWA2kmIDi4TIaP7LLvON0zA',
                          user_agent='my user agent')
 
-                         #print(reddit.read_only)  # Output: True
-    stickies = [reddit.subreddit("writingprompts").sticky(i).id for i in range(1,4)]
+    # print(reddit.read_only)  # Output: True
+    stickies = [reddit.subreddit("writingprompts").sticky(
+        i).id for i in range(1, 4)]
 
     for submission in reddit.subreddit('writingprompts').hot(limit=6):
         if submission.id not in stickies:
 
             date = datetime.utcfromtimestamp(submission.created_utc)
-            top_comment = [comment.body for comment in submission.comments if (hasattr(comment, "body") and comment.distinguished==None)][0]
+            top_comment = [comment.body for comment in submission.comments if (
+                hasattr(comment, "body") and comment.distinguished == None)][0]
             Writingprompt.objects.update_or_create(title=submission.title, comment=top_comment,
-            defaults={'score':submission.score, 'pub_date':date, 'url':submission.url},)
+                                                   defaults={'score': submission.score, 'pub_date': date, 'url': submission.url},)
 
     return 30
--- vivian/views.py
+++ vivian/views.py
@@ -8,20 +8,18 @@ import markdown2
 
 
 def index(request):
-    v=reddit()
-
-
+    v = reddit()
+    
 # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         # check whether it's valid:
 
-                latest_question_list = Writingprompt.objects.order_by("-score")
-                return render(request, 'vivian/index.html', {'question': latest_question_list, 'raw_comment':v})
+        latest_question_list = Writingprompt.objects.order_by("-score")[:5]
+        return render(request, 'vivian/index.html', {'question': latest_question_list, 'raw_comment': v})
 
-    # if a GET (or any other method) we'll create a blank form
     else:
         form = NameForm()
 
-    latest_question_list = Writingprompt.objects.order_by("-pub_date")
-    return render(request, 'vivian/index.html', {'question': latest_question_list, 'raw_comment':v})
+    latest_question_list = Writingprompt.objects.order_by("-pub_date")[:5]
+    return render(request, 'vivian/index.html', {'question': latest_question_list, 'raw_comment': v})
