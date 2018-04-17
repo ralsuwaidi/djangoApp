aef8ea2ff3afe3417c9ae6fdf0ff0ee37bd6238f
real4sight <x_suwaidi@gmail.com>
2018-04-17 19:20:14 +0400

    pragnate

--- vivian/templatetags/vivian_commands.py
+++ vivian/templatetags/vivian_commands.py
@@ -2,9 +2,14 @@ from django import template
 import praw
 import markdown2
 from django.template.defaultfilters import stringfilter
+from vivian.functions import reddit
 
 register = template.Library()
 
 @register.filter
 def markdown(value):
     return markdown2.markdown(value)
+
+@register.filter
+def populate_reddit(value):
+    return reddit()
