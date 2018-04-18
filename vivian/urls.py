c4753fe8d08c81dc0091da3e9f686a6a5d28daf5
real4sight <x_suwaidi@hotmail.com>
2018-04-18 10:25:28 +0400

    sorted

--- vivian/urls.py
+++ vivian/urls.py
@@ -5,5 +5,6 @@ from . import views
 pop_reddit()
 urlpatterns = [
     path('', views.index,  name='index'),
-    path('hot/', views.index,  name='index'),
+    path('hot/', views.hot,  name='hot'),
+    path('date/', views.date,  name='date'),
 ]
--- vivian/views.py
+++ vivian/views.py
@@ -8,13 +8,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 import markdown2
 
+pagn_num=3
 
 def index(request):
 
     pop_reddit()
 
     latest_question_list = Writingprompt.objects.order_by("-pub_date")
-    paginator = Paginator(latest_question_list, 5)
+    paginator = Paginator(latest_question_list, pagn_num)
 
     page = request.GET.get('page')
     contacts = paginator.get_page(page)
@@ -26,3 +27,21 @@ class IndexView(generic.ListView):
     paginate_by = 4
     context_object_name = 'question'
     template_name = 'vivian/index.html'
+
+def hot(request):
+
+    latest_question_list = Writingprompt.objects.order_by("-score")
+    paginator = Paginator(latest_question_list, pagn_num)
+
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'vivian/index.html', {'question': contacts, })
+
+def date(request):
+
+    latest_question_list = Writingprompt.objects.order_by("-pub_date")
+    paginator = Paginator(latest_question_list, pagn_num)
+
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+    return render(request, 'vivian/index.html', {'question': contacts, })
