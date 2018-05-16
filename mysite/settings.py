a61f84479e0da61fc80d7ca5f6bf1d2d23f89fc6
real4sight <x_suwaidi@hotmail.com>
2018-05-16 14:38:10 +0400

    fixed static

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates'), "D:/Programming/Python/MaterialUI/djangoApp/static/vivian/style.css",],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
