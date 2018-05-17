8e9ac8c8c76627ebde47614ca240a97cf385f530
real4sight <x_suwaidi@hotmail.com>
2018-05-17 12:13:33 +0400

    test

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index,  name='index'),
     path('languages/', views.languages,  name='languages'),
+     path('languages/your-name/', views.get_name,  name='name'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -7,3 +7,24 @@ def index(request):
 
 def languages(request):
     return render(request, 'dashboard/languages.html', )
+
+from .forms import NameForm
+from django.http import HttpResponseRedirect
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'dashboard/languages.html', {'form': form})
